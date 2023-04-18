a4bf584fba6ad456bff3c2e095aa40b2a694ad59
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-19 01:43:26 +0600

    String Concatenation

--- String_Concatenation.c
+++ String_Concatenation.c
@@ -23,25 +23,25 @@
 
 // concate half of the second string
 
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-    char s1[200],s2[100];
-    scanf("%s %s",s1,s2);
-    int i=strlen(s1),j;
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//     char s1[200],s2[100];
+//     scanf("%s %s",s1,s2);
+//     int i=strlen(s1),j;
      
-    for(int j=0;j<strlen(s2)/2;j++)
-    {
-        s1[i] = s2[j];
-        i++;
-    }
-    s1[i] = '\0';
+//     for(int j=0;j<strlen(s2)/2;j++)
+//     {
+//         s1[i] = s2[j];
+//         i++;
+//     }
+//     s1[i] = '\0';
 
-    printf("%s\n",s1);
+//     printf("%s\n",s1);
     
-    return 0;
-}
+//     return 0;
+// }
 
 //------------------------------------------------------------
 
