6380dae8a8f47927e167bdc42ff8a2e659725651
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-19 01:10:41 +0600

    String Lexicographical Comparison

---
+++ C_Compare.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+int main()
+{
+    char s1[100],s2[100];
+    scanf("%s %s",s1,s2);
+    int i=0;
+
+    while (1)
+    {
+        if(s1[i]=='\0' && s2[i]=='\0')
+        {
+            printf("%s\n",s1);// ekta dilei hbe(s1 or s2)
+            break;
+        }
+        else if(s1[i]=='\0')
+        {
+            printf("%s\n",s1);
+            break;
+        }
+        else if(s2[i]=='\0')
+        {
+            printf("%s\n",s2);
+            break;
+        }
+
+        if(s1[i]==s2[i]) i++;
+        else if(s1[i]<s2[i])
+        {
+            printf("%s\n",s1);
+            break;
+        }
+        else {
+            printf("%s\n",s2);
+            break;
+        }
+
+    }
+    
+    
+    return 0;
+}
+
+/*
+
+// #include<stdio.h>
+// int main()
+// {
+//     char s1[100],s2[100];
+//     scanf("%s %s",s1,s2);
+
+//     for (int i = 0;; i++)
+//     {
+//          if(s1[i]=='\0' && s2[i]=='\0')
+//         {
+//             printf("%s",s1);
+//             return 0;
+//         }
+//         if(s1[i]<s2[i])
+//         {
+//              printf("%s",s1);
+//              return 0;
+//         }
+//         if(s1[i]>s2[i])
+//         {
+//              printf("%s",s2);
+//              return 0;
+//         }
+
+//         if(s1[i]=='\0')
+//         {
+//              printf("%s",s1);
+//              return 0;
+//         }
+        
+//         if(s2[i]=='\0')
+//         {
+//              printf("%s",s2);
+//              return 0;
+//         }
+
+       
+//     }
+    
+    
+//     return 0;
+// }
+
+*/
